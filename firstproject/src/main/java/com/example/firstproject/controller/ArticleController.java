d7557efc22b0a858e7535c396939b210c1083ee8
SuuuHyeon <tngutnqls6909@gmail.com>
2023-12-18 16:06:00 +0900

    feat(main): DTO, Repository, Entity 생성

---
+++ firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -0,0 +1,31 @@
+package com.example.firstproject.controller;
+
+import com.example.firstproject.dto.ArticleForm;
+import com.example.firstproject.entity.Article;
+import com.example.firstproject.repository.ArticleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class ArticleController {
+    @Autowired // 스프링 부트가 미리 생성해 놓은 리파지터리 객체 주입(DI)
+    private ArticleRepository articleRepository;
+    @GetMapping("/articles/new")
+    public String newArticleForm() {
+        return "articles/new";
+    }
+
+    @PostMapping("/articles/create")
+    public String createArticle(ArticleForm form) {
+        System.out.println(form.toString());
+        Article article = form.toEntity();
+        System.out.println("DTO가 엔티티로 변환" + article.toString());
+        Article saved = articleRepository.save(article);
+        System.out.println("article을 DB로 변환" + saved.toString());
+        return "";
+    }
+
+
+}
