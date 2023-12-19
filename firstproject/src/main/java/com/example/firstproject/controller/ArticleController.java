870a9a7f29f0a2a73ac49e213bc13213f71f745b
SuuuHyeon <tngutnqls6909@gmail.com>
2023-12-19 15:21:41 +0900

    feat(lombok): 롬복 리펙토링

--- firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
+++ firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -3,11 +3,18 @@ package com.example.firstproject.controller;
 import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.entity.Article;
 import com.example.firstproject.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
 @Controller
 public class ArticleController {
     @Autowired // 스프링 부트가 미리 생성해 놓은 리파지터리 객체 주입(DI)
@@ -19,12 +26,34 @@ public class ArticleController {
 
     @PostMapping("/articles/create")
     public String createArticle(ArticleForm form) {
-        System.out.println(form.toString());
+//        System.out.println(form.toString());
         Article article = form.toEntity();
-        System.out.println("DTO가 엔티티로 변환" + article.toString());
+        log.info(article.toString());
+//        System.out.println("DTO가 엔티티로 변환" + article.toString());
         Article saved = articleRepository.save(article);
-        System.out.println("article을 DB로 변환" + saved.toString());
+        log.info(saved.toString());
+//        System.out.println("article을 DB로 변환" + saved.toString());
         return "";
+    }
+
+    @GetMapping("/articles/{id}")
+    public String show(@PathVariable Long id, Model model) {
+        // 1. id를 조회해 데이터 가져오기
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+        // 2. 모델에 데이터 등록하기
+        model.addAttribute("article", articleEntity);
+        log.info("id : " + id);
+        // 3. 뷰 페이지 반환하기
+        return "articles/show";
+    }
+    @GetMapping("/articles")
+    public String index(Model model) {
+        // 1. 모든 데이터 가져오기
+        ArrayList<Article> articleEntityList = articleRepository.findAll();
+        // 2. 모델에 데이터 등록
+        model.addAttribute("articleList", articleEntityList);
+        // 3. 뷰 페이지 설정
+        return "articles/index";
     }
 
 
--- firstproject/src/main/java/com/example/firstproject/controller/MemberController.java
+++ firstproject/src/main/java/com/example/firstproject/controller/MemberController.java
@@ -3,25 +3,33 @@ package com.example.firstproject.controller;
 import com.example.firstproject.dto.MemberForm;
 import com.example.firstproject.entity.Member;
 import com.example.firstproject.repository.MemberRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+@Slf4j
 @Controller
 public class MemberController {
     @Autowired
     private MemberRepository memberRepository;
-    @GetMapping("/members/register")
+    @GetMapping("/members/signUp")
     public String registerMember() {
-        return "members/register";
+        return "members/signUp";
     }
-    @PostMapping("/members/create")
+    @PostMapping("/members/join")
     public String CreateMember(MemberForm memberForm) {
         Member member = memberForm.toEntity();
-        System.out.println("member엔티티 : " + member.toString());
+        log.info("member엔티티 : " + member.toString());
         Member saved = memberRepository.save(member);
-        System.out.println("DB변환 : " + saved.toString());
+        log.info("DB변환 : " + saved);
         return "";
+    }
+
+    @GetMapping("/members/{id}")
+    public String show(@PathVariable Long id) {
+        return "members/show";
     }
 }
