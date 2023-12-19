1b04c89830d28004d3baab141d56aa518dd32ea0
SuuuHyeon <tngutnqls6909@gmail.com>
2023-12-19 19:20:47 +0900

    feat(member): 회원가입 실습

--- firstproject/src/main/java/com/example/firstproject/controller/MemberController.java
+++ firstproject/src/main/java/com/example/firstproject/controller/MemberController.java
@@ -6,9 +6,12 @@ import com.example.firstproject.repository.MemberRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
 
 @Slf4j
 @Controller
@@ -29,7 +32,18 @@ public class MemberController {
     }
 
     @GetMapping("/members/{id}")
-    public String show(@PathVariable Long id) {
+    public String show(@PathVariable Long id, Model model) {
+        Member memberEntity = memberRepository.findById(id).orElse(null);
+        model.addAttribute("member", memberEntity);
+        log.info("memberEntity : " + memberEntity);
         return "members/show";
+    }
+
+    @GetMapping("/members")
+    public String index(Model model) {
+        ArrayList<Member> memberEntityList = memberRepository.findAll();
+        model.addAttribute("memberEntityList", memberEntityList);
+        log.info("modelEntityList : " + memberEntityList.toString());
+        return "members/index";
     }
 }
